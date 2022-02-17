7d9ed251797a33086fc438003f77e6a48d332cb3
Anton <anton.goetzee-barral@airelogic.com>
2022-02-17 22:10:02 +0000

    section3 fix

--- jobs/app.py
+++ jobs/app.py
@@ -6,11 +6,11 @@ PATH="db/jobs.sqlite"
 app = Flask(__name__)
 
 def open_connection():
-    connection = getattr(g._connection, None)
+    connection = getattr(g, "_connection", None)
     if connection is None:
-        connection, g._connection  = sqlite.connect(PATH)
-        connection.row_factory = sqlite3.Row
-        return connection
+        connection = g._connection  = sqlite.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
@@ -19,11 +19,12 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
     return results
 
 @app.teardown_appcontext
 def close_connection(exception):
-    connection = getattr(g._connection, None)
+    connection = getattr(g, "_connection", None)
     if connection is not None:
         connection.close()
 
