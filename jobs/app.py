0a674bc014f90148b2f09c6a9f7c2ed740df0414
Anton <anton.goetzee-barral@airelogic.com>
2022-02-18 19:26:36 +0000

    section8

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, g
+import datetime
+from flask import Flask, render_template, g, request, redirect, url_for
 import sqlite3
 
 PATH="db/jobs.sqlite"
@@ -65,3 +66,19 @@ def employer(employer_id):
         employer=employer, 
         jobs=jobs,
         reviews=reviews)
+
+@app.route("/employer/<employer_id>/review", methods=("GET", "POST"))
+def review(employer_id):
+    if request.method == "POST":
+        review = request.form["review"]
+        rating = request.form["rating"]
+        title = request.form["title"]
+        status = request.form["status"]
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql(
+            "INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)",
+            (review, rating, title, date, status, employer_id),
+            commit=True
+        )
+        return redirect(url_for(employer=employer_id))
+    render_template("review.html", employer_id=employer_id)
